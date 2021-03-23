7e2b40b4cc61d14fea6f8390cedb8a196e0f6740
Iberassa <indriasbelachew@gmail.com>
2021-03-23 18:13:12 -0400

    Day 2

--- CS303/labW1D2/app.js
+++ CS303/labW1D2/app.js
@@ -1,4 +1,21 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D2";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+// See tests.js for behavior
+function nameString(obj) {
+    if (obj == 0) {
+        return "object has no properties";
+    }
+    if (obj === undefined) {
+        return "no object passed";
+    } else if (obj === null) {
+        return "null argument"
+    } else if (obj !== 0) {
+        let arr = [];
+        for (let elem in obj) {
+            arr.push(elem);
+        }
+        arr.sort();
+        return arr.join(".");
+    }
 }
